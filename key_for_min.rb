56b163bd92bc0cb5ba7f72dea7631703ae5a99bd
Sherry <sherryptk@gmail.com>
2017-11-10 00:49:56 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,22 @@
 
 def key_for_min_value(name_hash)
 
+lowest = 100000000
+
+  if name_hash == {}
+    return nil
+  end
+  
+   name_hash.collect do |name, age|
+     if age < lowest
+       lowest = age
+     end
+  end
+
+  name_hash.each do |name, age|
+    if lowest == age
+      return name
+    end
+  end
+
 end
